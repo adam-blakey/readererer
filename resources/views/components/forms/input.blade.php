652e5e559930a2ac4153f6dfdbca088993a9c8e7
Adam Blakey <adam@blakey.family>
2024-07-12 23:31:09 +0100

    +

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -3,7 +3,7 @@
 <div class="mb-3">
     <label class="form-label {{ $required?'required':'' }}">{{ $title }}</label>
     <div>
-        <input type="{{ $type }}" class="form-control" aria-describedby="emailHelp" placeholder="{{ $placeholder }}" id="{{ $name }}" name="{{ $name }}" {{ $required?'required':'' }}>
+        <input type="{{ $type }}" class="form-control" placeholder="{{ $placeholder }}" id="{{ $name }}" name="{{ $name }}" {{ $required?'required':'' }}>
         @if($hint != '')
             <small class="form-hint">{{ $hint }}</small>
         @endif
