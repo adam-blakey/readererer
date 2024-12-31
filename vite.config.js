fbe4f88ce9836c01db818851370fea2e60422be1
Adam Blakey <adam@blakey.family>
2024-12-31 18:11:46 +0000

    Amended Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/images/favicon.png', 'resources/images/readererer-long-logo.svg', 'resources/images/readererer-square-logo.svg'],
             refresh: true,
         }),
     ],
