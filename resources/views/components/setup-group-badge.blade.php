4fb25a244614ab8beecf1279c077c011cd09232d
Adam Blakey <adam@blakey.family>
2026-04-03 20:34:50 +0100

    Tweak to setup group badge.

--- resources/views/components/setup-group-badge.blade.php
+++ resources/views/components/setup-group-badge.blade.php
@@ -5,5 +5,5 @@
 @elseif ($show_as_dot)
     <span class="badge badge-dot bg-{{ $setup_group->color }} badge-notification"></span>
 @else
-    <span class="badge badge-lg bg-{{ $setup_group->color }} text-{{ $setup_group->color }}-fg">{{ $setup_group->name }}</span>
+    <span class="badge badge-lg bg-{{ $setup_group->color }} text-{{ $setup_group->color }}-fg">{{ $setup_group->week }}</span>
 @endif
