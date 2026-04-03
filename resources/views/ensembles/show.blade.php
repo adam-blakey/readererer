53da573ebe36c7b9eb0805ebf1d7fb8614bd0aa5
Adam Blakey <adam@blakey.family>
2026-04-03 14:03:06 +0100

    Replaced raw SVG icons in ensemble show view.

--- resources/views/ensembles/show.blade.php
+++ resources/views/ensembles/show.blade.php
@@ -69,31 +69,14 @@
 								<div class="card-body">
 									<div class="card-title">Member info</div>
 									<div class="mb-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-cog" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                           <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                           <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
-                                           <path d="M6 21v-2a4 4 0 0 1 4 -4h2.5"></path>
-                                           <path d="M19.001 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-                                           <path d="M19.001 15.5v1.5"></path>
-                                           <path d="M19.001 21v1.5"></path>
-                                           <path d="M22.032 17.25l-1.299 .75"></path>
-                                           <path d="M17.27 20.75l-1.3 .75"></path>
-                                           <path d="M15.97 17.25l1.3 .75"></path>
-                                           <path d="M20.733 20.75l1.3 .75"></path>
-                                        </svg>
+                                        <x-icon name="user-cog" />
 										Admins ({{ $ensemble->admins->count() }}):
 										@foreach ($ensemble->admins as $admin)
 											<x-user-entry :user="$admin" />
 										@endforeach
 									</div>
 									<div class="mb-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                           <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                           <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
-                                           <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
-                                           <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-                                           <path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path>
-                                        </svg>
+                                        <x-icon name="users" />
 										Members ({{ $ensemble->users->count() }}):
 										@foreach ($ensemble->users as $user)
 											<x-user-entry :user="$user" />
